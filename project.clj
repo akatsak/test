c90d05232b17ef4e60585502ffa7d6644c494912
akatsak <nikbertov@gmail.com>
2016-06-04 19:38:30 +0700

    New File 2

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject lein-test "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main lein-test.test)
